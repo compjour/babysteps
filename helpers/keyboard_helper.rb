c9bc8e56101f56950711a66609630025e8e2e181
Dan Nguyen <dansonguyen@gmail.com>
2015-09-14 13:27:15 -0700

    gif madness

--- helpers/keyboard_helper.rb
+++ helpers/keyboard_helper.rb
@@ -8,9 +8,10 @@ end
 
 
 def kbset(*key_vals)
+  ckeys = key_vals[0].is_a?(Array) ? key_vals[0] : key_vals
   content_tag('div', :class => 'kbs') do
     s = ActiveSupport::SafeBuffer.new
-    keys_html = key_vals.map{ |kval| kbkey(kval) }
+    keys_html = ckeys.map{ |kval| kbkey(kval) }
     s.safe_concat keys_html.join(kb_plus)
   end
 end
@@ -21,10 +22,15 @@ KbSpecialKeys = {
   "Tab" => %Q{<span class="kbkey tab" data-value="Tab">tab</span>},
   "Alt" => %Q{<span value="Alt" class="kbkey alt" data-value="Alt"><span class="symbol">alt</span>option</span>},
   "Command" => %Q(<span class="kbkey cmd" data-value="Command"><span class="symbol">⌘</span>command</span>),
-  "Shift" => %Q{<span class="kbkey shift" data-value="Shift">shift</span>}
+  "Shift" => %Q{<span class="kbkey shift" data-value="Shift">shift</span>},
+  "Right" => %Q{<span class="kbkey right" data-value="Right">►</span>},
+  "Left" => %Q{<span class="kbkey left" data-value="Left">◄</span>},
+  "~" => %Q{<span class="kbkey tilde" data-value="~"><span class="symbol">~</span>`</span>}
+
+
 }
 
-
+# ▼▲
 
 
 def kb_plus
